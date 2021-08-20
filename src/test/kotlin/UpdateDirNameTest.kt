ebd1817024738b0a883180097076eea73ee317c2
MR3Y <abdonasr379@gmail.com>
2021-08-20 03:13:55 +0200

    Expand UpdateDirNameTest coverage

--- src/test/kotlin/UpdateDirNameTest.kt
+++ src/test/kotlin/UpdateDirNameTest.kt
@@ -1,7 +1,7 @@
 import org.junit.Test
-import utils.root
-import java.io.File
-import kotlin.io.path.Path
+import utils.baseMainPath
+import utils.getModuleMainDirs
+import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.io.path.exists
 import kotlin.io.path.notExists
@@ -13,16 +13,47 @@ import kotlin.test.assertTrue
 class UpdateDirNameTest {
 
     @Test
-    fun `check base module directories are updated correctly`() {
-        val (from, to) = "com.project" to "net.bar"
-        val baseModulePath = root / "base" / "src" / "main" / "kotlin"
-        val dirs = File(baseModulePath.toString()).walkBottomUp().filter { it.isDirectory }
+    fun `check app module directories are updated correctly`() {
+        val oldPath = baseMainPath("app") / "kotlin" / "com" / "project" / "app"
+        val newPath = baseMainPath("app") / "kotlin" / "net" / "bar" / "foo"
+        runTest("app", "com.project.app", "net.bar.foo", oldPath, newPath)
+    }
+
+    @Test
+    fun `check base module parent dirs are updated correctly`() {
+        val oldPath = baseMainPath("base") / "kotlin" / "com" / "project" / "base" / "project"
+        val newPath = baseMainPath("base") / "kotlin" / "io" / "foo" / "base" / "project"
+        runTest("base", "com.project", "io.foo", oldPath, newPath)
+    }
+
+    @Test
+    fun `check base module single duplicated segment is updated in multiple places`() {
+        val oldPath = baseMainPath("base") / "kotlin" / "com" / "project" / "base" / "project"
+        val newPath = baseMainPath("base") / "kotlin" / "com" / "foo" / "base" / "foo"
+        runTest("base", "project", "foo", oldPath, newPath)
+    }
+
+    @Test
+    fun `check base module multiple duplicated segment is mapped correctly`() {
+        val oldPath = baseMainPath("base") / "kotlin" / "com" / "project" / "base" / "project"
+        val newPath = baseMainPath("base") / "kotlin" / "com" / "pro" / "foo" / "io"
+        runTest("base", "project.base.project", "pro.foo.io", oldPath, newPath)
+    }
+
+    @Test
+    fun `check common module's part of dir is updated correctly`() {
+        val oldPath = baseMainPath("common") / "kotlin" / "com" / "project" / "common" / "app"
+        val newPath = baseMainPath("common") / "kotlin" / "com" / "prcrowct" / "common" / "app"
+        runTest("common", "oje", "crow", oldPath, newPath)
+    }
+
+    private fun runTest(module: String, from: String, to: String, oldPath: Path, newPath: Path) {
+        val dirs = getModuleMainDirs(module)
         dirs.forEach { it.updateDirName(from, to) }
-        val (oldPath, newPath) = Path("com") / "project" / "base" to Path("net") / "bar" / "base"
-        assertTrue { (baseModulePath / newPath / "project").exists() }
-        assertTrue { (baseModulePath / newPath / "util").exists() }
-        assertTrue { (baseModulePath / oldPath / "project").notExists() }
-        assertTrue { (baseModulePath / oldPath / "util").notExists() }
+        assertTrue { newPath.exists() }
+        assertTrue { newPath.exists() }
+        assertTrue { oldPath.notExists() }
+        assertTrue { oldPath.notExists() }
         // reset to default
         dirs.forEach { it.updateDirName(to, from) }
     }
--- src/test/kotlin/utils/TestUtils.kt
+++ src/test/kotlin/utils/TestUtils.kt
@@ -1,12 +1,20 @@
 package utils
 
 import java.io.File
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.div
 
 internal val root = Paths.get("").toAbsolutePath() / "src" / "test" / "fakeproject"
 
+private val srcDir: (String) -> Path = { module -> root / module / "src" / "main" }
+
+internal val baseMainPath: (String) -> Path = { module -> root / module / "src" / "main" }
+
 internal fun getModuleMainFiles(module: String): Sequence<File> {
-    val srcDir = root / module / "src" / "main"
-    return File(srcDir.toString()).walkBottomUp().filter { it.isFile }
+    return File(srcDir(module).toString()).walkBottomUp().filter { it.isFile }
+}
+
+internal fun getModuleMainDirs(module: String): Sequence<File> {
+    return File(srcDir(module).toString()).walkBottomUp().filter { it.isDirectory }
 }
